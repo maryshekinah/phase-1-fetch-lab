6f2b0413a70977d04a7a43c90f5208d0ad56ddae
shekinah <nyamburamary1212@gmail.com>
2023-09-29 00:10:19 +0300

    complete fetch lab

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+    // Use the fetch function to make a request to the API
+    return fetch('https://anapioficeandfire.com/api/books')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse the JSON response
+      })
+      .then((data) => {
+        // Once the data is fetched and parsed, call the renderBooks function
+        renderBooks(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }
   
-}
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
